eb6abb1fc897c86c246e868734a651c9f717a86e
PawanNayanajith <djnayanajith@gmail.com>
2018-09-16 14:48:04 +0530

    Querydsl-JPA

---
+++ src/main/java/com/sgic/myleave/criteria/UserCriteria.java
@@ -0,0 +1,38 @@
+package com.sgic.myleave.criteria;
+
+public class UserCriteria {
+
+	private String userNaeme;
+	private String leaveName;
+	private Integer leaveAllocation;
+	private String telephone;
+	
+	public String getUserNaeme() {
+		return userNaeme;
+	}
+	public void setUserNaeme(String userNaeme) {
+		this.userNaeme = userNaeme;
+	}
+	public String getLeaveName() {
+		return leaveName;
+	}
+	public void setLeaveName(String leaveName) {
+		this.leaveName = leaveName;
+	}
+	public Integer getLeaveApplication() {
+		return leaveAllocation;
+	}
+	public void setLeaveApplication(Integer leaveApplication) {
+		this.leaveAllocation = leaveApplication;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+	
+	
+}
